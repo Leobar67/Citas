7e788aad395539aa6ed68fd35bc39328b882e7b4
Leobar67 <158235574+Leobar67@users.noreply.github.com>
2026-01-28 15:03:43 -0600

    Refactor obtener_horas.php to use PDO

--- php/obtener_horas.php
+++ php/obtener_horas.php
@@ -3,15 +3,8 @@ include("../conexion.php");
 
 $fecha = $_GET['fecha'];
 
-$sql = $conexion->prepare("SELECT hora FROM citas WHERE fecha = ?");
-$sql->bind_param("s", $fecha);
-$sql->execute();
-
-$resultado = $sql->get_result();
-$horas = [];
-
-while ($fila = $resultado->fetch_assoc()) {
-    $horas[] = $fila['hora'];
-}
+$stmt = $conexion->prepare("SELECT hora FROM citas WHERE fecha = :fecha");
+$stmt->execute([':fecha' => $fecha]);
+$horas = $stmt->fetchAll(PDO::FETCH_COLUMN);
 
 echo json_encode($horas);
