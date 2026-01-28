8e7da1c35f90916f73daf8eee4d121e6d007b4bf
Leobar67 <158235574+Leobar67@users.noreply.github.com>
2026-01-28 15:04:00 -0600

    Refactor obtener_turno.php for cleaner code

--- php/obtener_turno.php
+++ php/obtener_turno.php
@@ -1,26 +1,13 @@
 <?php
 include("../conexion.php");
 
-$response = [
-    "inicio" => "08:00",
-    "fin"    => "15:30"
-];
+$response = ["inicio" => "08:00", "fin" => "15:30"];
 
-$inicio = $conexion->query(
-    "SELECT valor FROM configuracion WHERE tipo='turno_inicio' LIMIT 1"
-);
+$inicio = $conexion->query("SELECT valor FROM configuracion WHERE tipo='turno_inicio' LIMIT 1")->fetch(PDO::FETCH_ASSOC);
+$fin    = $conexion->query("SELECT valor FROM configuracion WHERE tipo='turno_fin' LIMIT 1")->fetch(PDO::FETCH_ASSOC);
 
-$fin = $conexion->query(
-    "SELECT valor FROM configuracion WHERE tipo='turno_fin' LIMIT 1"
-);
-
-if ($inicio && $inicio->num_rows > 0) {
-    $response['inicio'] = $inicio->fetch_assoc()['valor'];
-}
-
-if ($fin && $fin->num_rows > 0) {
-    $response['fin'] = $fin->fetch_assoc()['valor'];
-}
+if ($inicio) $response['inicio'] = $inicio['valor'];
+if ($fin)    $response['fin'] = $fin['valor'];
 
 header('Content-Type: application/json');
 echo json_encode($response);
